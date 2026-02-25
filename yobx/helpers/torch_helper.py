54dd4d26b5360cd8d3d64d4a00b9bf234e127b6a
Xavier Dupré <xadupre@microsoft.com>
2026-02-25 23:41:19 +0100

    fix

--- yobx/helpers/torch_helper.py
+++ yobx/helpers/torch_helper.py
@@ -1,5 +1,6 @@
 import onnx
 import torch
+from .onnx_helper import onnx_dtype_name
 
 _TYPENAME = dict(
     FLOAT=onnx.TensorProto.FLOAT,
@@ -8,6 +9,48 @@ _TYPENAME = dict(
     FLOAT16=onnx.TensorProto.FLOAT16,
     BFLOAT16=onnx.TensorProto.BFLOAT16,
 )
+
+
+def onnx_dtype_to_torch_dtype(itype: int) -> torch.dtype:
+    """
+    Converts an onnx type into a torch dtype.
+
+    :param to: onnx dtype
+    :return: torch dtype
+    """
+    if itype == onnx.TensorProto.FLOAT:
+        return torch.float32
+    if itype == onnx.TensorProto.FLOAT16:
+        return torch.float16
+    if itype == onnx.TensorProto.BFLOAT16:
+        return torch.bfloat16
+    if itype == onnx.TensorProto.DOUBLE:
+        return torch.float64
+    if itype == onnx.TensorProto.INT32:
+        return torch.int32
+    if itype == onnx.TensorProto.INT64:
+        return torch.int64
+    if itype == onnx.TensorProto.UINT32:
+        return torch.uint32
+    if itype == onnx.TensorProto.UINT64:
+        return torch.uint64
+    if itype == onnx.TensorProto.BOOL:
+        return torch.bool
+    if itype == onnx.TensorProto.INT16:
+        return torch.int16
+    if itype == onnx.TensorProto.UINT16:
+        return torch.uint16
+    if itype == onnx.TensorProto.INT8:
+        return torch.int8
+    if itype == onnx.TensorProto.UINT8:
+        return torch.uint8
+    if itype == onnx.TensorProto.COMPLEX64:
+        return torch.complex64
+    if itype == onnx.TensorProto.COMPLEX128:
+        return torch.complex128
+    raise NotImplementedError(
+        f"Unable to convert onnx type {onnx_dtype_name(itype)} to torch.type."
+    )
 
 
 def torch_dtype_to_onnx_dtype(to: torch.dtype) -> int:
@@ -57,4 +100,6 @@ def torch_dtype_to_onnx_dtype(to: torch.dtype) -> int:
     sto = str(to)
     if sto in _TYPENAME:
         return _TYPENAME[sto]
-    raise NotImplementedError(f"Unable to convert torch dtype {to!r} ({type(to)}) to onnx dtype.")
+    raise NotImplementedError(
+        f"Unable to convert torch dtype {onnx_dtype_name(to)} ({type(to)}) to onnx dtype."
+    )
