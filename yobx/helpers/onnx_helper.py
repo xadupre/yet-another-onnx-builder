e8ce2c4eaffbfb9228d87f0419393f4505968e6c
Xavier Dupré <xadupre@microsoft.com>
2026-02-25 22:42:32 +0100

    add dtype_to_tensor_dtype

--- yobx/helpers/onnx_helper.py
+++ yobx/helpers/onnx_helper.py
@@ -1,3 +1,4 @@
+from typing import Union
 import numpy as np
 import onnx.helper as oh
 
@@ -5,3 +6,19 @@ import onnx.helper as oh
 def np_dtype_to_tensor_dtype(dtype: np.dtype) -> int:
     """Converts a numpy dtype to an onnx element type."""
     return oh.np_dtype_to_tensor_dtype(dtype)
+
+
+def dtype_to_tensor_dtype(dt: Union[np.dtype, "torch.dtype"]) -> int:  # noqa: F821
+    """
+    Converts a torch dtype or numpy dtype into a onnx element type.
+
+    :param to: dtype
+    :return: onnx type
+    """
+    try:
+        return np_dtype_to_tensor_dtype(dt)
+    except (KeyError, TypeError, ValueError):
+        pass
+    from .torch_helper import torch_dtype_to_onnx_dtype
+
+    return torch_dtype_to_onnx_dtype(dt)
