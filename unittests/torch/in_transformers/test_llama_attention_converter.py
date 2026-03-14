4fd5446042964a060175d40c8ec362ed2e61a8b6
Xavier Dupré <xadupre@microsoft.com>
2026-03-14 23:05:26 +0100

    fix

--- unittests/torch/in_transformers/test_llama_attention_converter.py
+++ unittests/torch/in_transformers/test_llama_attention_converter.py
@@ -76,7 +76,7 @@ def _to_model(attn, hs, cos, sin, mask=None, target_opset=22):
     return g.to_onnx(optimize=False)
 
 
-@requires_transformers("4.50")
+@requires_transformers("4.57")
 class TestLlamaAttentionConverter(ExtTestCase):
     """Unit tests for :func:`llama_attention_to_onnx`."""
 
