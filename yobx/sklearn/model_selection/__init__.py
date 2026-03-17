314830c9eeadf585d3e92bd73d203422dd42e532
Xavier Dupré <xadupre@microsoft.com>
2026-03-17 16:48:11 +0100

    try

--- yobx/sklearn/model_selection/__init__.py
+++ yobx/sklearn/model_selection/__init__.py
@@ -2,4 +2,9 @@ def register():
     from . import grid_search  # noqa: F401
     from . import halving_search  # noqa: F401
     from . import random_search  # noqa: F401
-    from . import tuned_threshold_classifier_cv  # noqa: F401
+
+    try:  # noqa: SIM105
+        from . import tuned_threshold_classifier_cv  # noqa: F401
+    except ImportError:
+        # This was introducted in scikit-learn==1.5
+        pass
